7f3fab6750198edcbef60d476699f845cd250cc7
finmook <apimooksarovath@gmail.com>
2025-12-10 10:26:05 +0700

    feat:prepare for backend production

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ import prisma from "./lib/prisma.js";
 import cors from "cors";
 const app = express();
 app.use(cors({
-  origin: "http://localhost:3000",   
+  origin: "*",   
   methods: ["GET", "POST"],
   credentials: true
 }));
